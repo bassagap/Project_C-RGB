f1d119630447a7c5e827e212ac5649e7579e21a2
Paula Bassagañas <paula.bassaganas@roche.com>
2016-10-16 22:58:57 +0200

    [16.10.2016] Modifications on configuration template, as well as in sensors template. Adding states to app.js and the configuration needed to display the tabs on the bottom.

--- Project_C-RGB/www/app/app.js
+++ Project_C-RGB/www/app/app.js
@@ -1,4 +1,6 @@
-﻿(function () {
+﻿
+(function () {
+    //[16.10.2016] bassagap: adding the ioncConfigProvider in order to place the tabs at the bottom of the application, adding templates states. 
     "use strict";
 
     angular.module("myapp", ["ionic", "myapp.controllers", "myapp.services"])
@@ -12,33 +14,38 @@
                 }
             });
         })
-        .config(function ($stateProvider, $urlRouterProvider) {
+        .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
             $stateProvider
             .state("app", {
                 url: "/app",
                 abstract: true,
-                templateUrl: "app/templates/view-menu.html",
+                templateUrl: "app/templates/menu/view-menu.html",
                 controller: "appCtrl"
             })
             .state("app.home", {
                 url: "/home",
-                templateUrl: "app/templates/view-home.html",
+                templateUrl: "app/templates/home/view-home.html",
                 controller: "homeCtrl"
             })
             .state("account", {
                 url: '/account',
-                templateUrl: 'app/templates/account.html'
+                templateUrl: 'app/templates/menu/account.html'
             })
-            .state("calendar", {
+            .state("callibration", {
                 url: '/calendar',
-                templateUrl: 'app/templates/calendar.html'
+                templateUrl: 'app/templates/menu/calibration.html'
             })
-            .state("notifications", {
-                url: '/notifications',
-                templateUrl: 'app/templates/notifications.html'
+            .state("configuration", {
+                url: '/configuration',
+                templateUrl: 'app/templates/menu/configuration.html'
+            })
+            .state("sensors", {
+                url: '/sensors',
+                templateUrl: 'app/templates/menu/sensors.html'
             })
                
             $urlRouterProvider.otherwise("/app/home");
+            $ionicConfigProvider.tabs.position('bottom');   
         });
     //.controller('NavCtrl', function($scope, $ionicSideMenuDelegate) {
     //    $scope.showMenu = function () {
